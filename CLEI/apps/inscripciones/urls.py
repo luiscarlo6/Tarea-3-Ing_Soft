97431e42b8c2453d5fb1c3cccb5feab945ecd6e7
luiscarlo6 <luiscarlo6@gmail.com>
2013-12-17 19:40:05 -0430

    Paginas listas para agregar Participante

--- CLEI/apps/inscripciones/urls.py
+++ CLEI/apps/inscripciones/urls.py
@@ -1,18 +1,14 @@
-from CLEI.apps.inscripciones.views import CreateParticipanteView, \
-    VerParticipanteView, SelectPaqueteView
 from django.conf.urls import patterns, url
+from CLEI.apps.inscripciones.views import CreateParticipanteView,\
+    VerParticipanteView
 
 
 urlpatterns = patterns('CLEI.apps.inscripciones.views',
-   url(r'^$', 'index_view' , name='vista_principal'),
-   
-   url(r'^create/$', CreateParticipanteView.as_view(),
+                       url(r'^$', 'index_view', name='vista_principal'),
+                       url(r'^paquete/$', 'select_paquete_view', name='vista_seleccion_paquete'),
+                       url(r'^descuento/$', 'select_descuento_view', name='vista_seleccion_descuento'),
+                       url(r'^create/$', CreateParticipanteView.as_view(), 
         name='crear_participante'),
-                       
-   url(r'^ver/(?P<pk>[\w]+)/$', VerParticipanteView.as_view(),
+                        url(r'^ver/(?P<pk>[\w]+)/$', VerParticipanteView.as_view(), 
         name='ver_participante'),
-                       
-   url(r'^select/$', SelectPaqueteView.as_view(),
-        name='seleccionar_paquete'),
-    
-)
+                       )
--- CLEI/apps/inscripciones/views.py
+++ CLEI/apps/inscripciones/views.py
@@ -1,28 +1,23 @@
-from CLEI.apps.inscripciones.forms import ParticipanteForm
-from CLEI.apps.inscripciones.models import Participante
-from django.core.urlresolvers import reverse
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView
+from CLEI.apps.inscripciones.models import Participante
+from CLEI.apps.inscripciones.forms import ParticipanteForm
+from django.core.urlresolvers import reverse
+from django.views.generic.detail import DetailView
 
 
-# Create your views here.
 def index_view(request):
     return render_to_response('inscripciones/index.html',
-                               context_instance=RequestContext(request))
+                              context_instance = RequestContext(request))
 
-class SelectPaqueteView(CreateView):
-    
-    template_name = "inscripciones/select_paquete.html"
-    def get_context_data(self, *args, **kwargs):
-        context = super(SelectPaqueteView, self).get_context_data(*args, **kwargs)
+def select_paquete_view(request):
+    return render_to_response('inscripciones/select_paquete.html',
+                              context_instance = RequestContext(request))
 
-        return context
-
-    def get_success_url(self):
-        return reverse('crear_participante', args=[self.object.id])
-
+def select_descuento_view(request):
+    return render_to_response('inscripciones/select_descuento.html',
+                              context_instance = RequestContext(request))
 class CreateParticipanteView(CreateView):
     model = Participante
     form_class = ParticipanteForm
@@ -30,13 +25,11 @@ class CreateParticipanteView(CreateView):
 
     def get_context_data(self, *args, **kwargs):
         context = super(CreateParticipanteView, self).get_context_data(*args, **kwargs)
-
         return context
 
     def get_success_url(self):
-        return reverse('ver_participante', args=[self.object.id])
+        return reverse('ver_participante',args=[self.object.id])
 
 class VerParticipanteView(DetailView):
     model = Participante
     template_name = "inscripciones/ver_participante.html"
-    
